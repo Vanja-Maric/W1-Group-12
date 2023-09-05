012c813dddd332950bdaa2c63f30be80dda99ea6
Vanja-Maric <vm222hx@student.lnu.se>
2023-09-05 10:31:22 +0200

    buttons listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 let display = document.getElementById('display').firstElementChild
 let numbers = document.getElementById('numbers').children
+console.log(numbers)
+
+for (let index = 0; index < numbers.length; index++) {
+  numbers[index].addEventListener('click', print(numbers[index]))
+}
+
+function print (button) {
+  console.log(button.innerText)
+}
